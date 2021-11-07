460a024e145733219eb52144d4b0fd8287168040
“jelin.mi” <jelin.mi@gmail.com>
2021-11-07 18:08:33 +0100

    cardsFlip for non-pairs

--- js/main.js
+++ js/main.js
@@ -17,9 +17,10 @@ window.addEventListener("load", function () {
   const tweety = generateCard (1, "tweety");
   const minnie = generateCard (2, "minnie");
   const mickey = generateCard (2, "mickey");
+  const back = generateCard (3, "back");
 
   // Create game instance
-  const game = new Game([sylvester, tweety, minnie, mickey]);
+  const game = new Game([sylvester, tweety, minnie, mickey, back]);
   
   playButton.addEventListener("click", function () {
     splashScreen.classList.add("hidden");
@@ -66,39 +67,91 @@ class Game {
     this.scoreCounting = score;
     this.time = document.querySelector(".time span");
     this.score = document.querySelector(".score span");
+    this.selectedCard = null;
+    this.solvedPairs = []; //[21, 1, 2,....] list of partnerId
   }
 
   prepareCards(){
 
-    /* const listCards = document.querySelector("#cards"); */
-    console.log(this.cards);
-    this.cards.forEach(function(item){
+    this.cards.forEach((item) => {
       const parent = document.querySelector("#cards");
-      const card = document.createElement("div");
-      console.log("HERE", item.name);
+      const cardHTML = document.createElement("div");
+      //Add name as an Id for the cardHTML
+      cardHTML.id = item.name;
+  
 
-      card.innerHTML = `
+      cardHTML.innerHTML = `
       <div data-id="${item.partnerId}" class="card visible">
-        <div class="flip-card-inner">
-          <div class="card-back card-look">
+        <div class="flip-card-inner" >
+          <div class="card-front card-look hidden">
             <img src="${item.image}" />
           </div>
-          <div class="card-front card-look">
+          <div class="card-back card-look ">
             <img src="../img/back.png" />
           </div>
         </div>
       </div>`;
-      parent.appendChild(card);
+
+
+
+      cardHTML.addEventListener('click',  ()=>{
+        const frontCard = cardHTML.querySelector(".card-front");
+        const backCard = cardHTML.querySelector(".card-back");
+        frontCard.classList.remove("hidden");
+        backCard.classList.add("hidden");
+
+        if (!!this.selectedCard){
+          if(item.partnerId === this.selectedCard.partnerId){
+            // when 2 cards match - you found the partner
+            this.solvedPairs.push(item.partnerId);
+            this.selectedCard = null;
+          } else {
+            // when 2 cards do match - you did not find the partner
+
+            //Get name of selectedCard to look for a Div with the same Id (div id = name)
+            let selectedName = this.selectedCard.name;
+            this.selectedCard = null;
+            // set Timeout to show the answer and hide it again
+            setTimeout(function(){
+
+              frontCard.classList.add("hidden")
+              backCard.classList.remove("hidden");
+              //Get DOM element that corresponds to the selectedCard object
+              const tweety = document.querySelector(`#${selectedName}`);
+              
+              const frontCardTweety = tweety.querySelector(".card-front");
+              const backCardTweety = tweety.querySelector(".card-back");
+
+              //Hide DOM element that corresponds to the selectedCard object
+              frontCardTweety.classList.add("hidden");
+              backCardTweety.classList.remove("hidden");
+
+              //reset selectedCard
+              
+
+            }, 1000);
+            
+          }
+        } else {
+          this.selectedCard = item; // first click - if there is no selected Card, add this card (item) to selectedCard variable 
+        }
+       
+      });
+
+      parent.appendChild(cardHTML);
     });
   }
-  
+  isMatch(card){
+    this.selectedCard = card;
+    console.log(card)
+  }
   randomCards(){}
 
   startTimer(){}
 
   updateScore(){}
 
-  flipCard(){
+ /*  flipCard(){
     let cardFront = document.querySelector("card-front");
     let cardBack = document.querySelector("card-back");
 
@@ -110,9 +163,9 @@ class Game {
       cardFront.classList.add("visible");
       cardBack.classList.remove("visible");
     });
-  }
+  } */
 
-  isMatch(){}
+  
 }
 
 
